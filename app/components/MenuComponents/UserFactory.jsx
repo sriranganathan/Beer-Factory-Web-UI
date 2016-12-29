1f6e3e827836fe3e0e263547b0a81f9d790749b8
sriranganathan <ravisriranganathan@gmail.com>
2016-12-29 23:48:05 +0530

    Update UserFactory Component

--- app/components/MenuComponents/UserFactory.jsx
+++ app/components/MenuComponents/UserFactory.jsx
@@ -4,9 +4,17 @@ var {connect} = require('react-redux');
 var UserFactory = React.createClass({
 
     render: function () {
+        var {factory} = this.props
         return (
             <div>
                 <center><h3>Factory</h3></center>
+                <hr />
+                <div>
+                    <p>Total Capacity : {factory.capacity}</p>
+                    <p>Used : {factory.used}</p>
+                    <p>Available : {factory.capacity-factory.used}</p>
+                    <p></p>
+                </div>
             </div>
         );
     }
