4588871d44a79a00a4ebacf19aea4b50de1ca0c2
sriranganathan <ravisriranganathan@gmail.com>
2017-01-01 00:54:15 +0530

    Add skip turn feature
    - Add skip turn feature
    
    fixes https://github.com/sriranganathan/Beer-Factory-Web-UI/issues/5

--- app/components/MenuElement.jsx
+++ app/components/MenuElement.jsx
@@ -2,9 +2,13 @@ var React = require('react');
 var {connect} = require('react-redux');
 var UserFactory = require('UserFactory');
 var Retailer = require('Retailer');
-var {Row, Column, Icon} = require('react-foundation');
+var {Row, Column} = require('react-foundation');
 var Inactive = require('Inactive');
-var {convertHrtoDays} = require('helpers');
+var {initiateReset, convertHrtoDays} = require('helpers');
+var API = require('API');
+var {toastr} = require('react-redux-toastr');
+var advanceTurn = require('advanceTurn');
+var {showLoading, hideLoading} = require('Actions');
 
 var MenuElement = React.createClass({
 
@@ -30,15 +34,43 @@ var MenuElement = React.createClass({
 
   },
 
+  handleClick: function () {
+    var {user_id, auth_token, dispatch} = this.props;
+    var data = {
+      user_id,
+      auth_token
+    };
+    
+    var success = (data) => {
+      advanceTurn({user_id, auth_token}, dispatch);
+      toastr.success('Success', 'Skipped Turn Successfully');
+    }
+
+    var failure = (error) => {
+      var msg = error.message;
+      if(msg === '401') {
+        initiateReset(this.props.dispatch);
+        toastr.error('Invalid Credentials', 'Please Login Again');
+      } else {
+        toastr.error('Error', msg);
+        dispatch(hideLoading());
+      }
+    };
+
+    dispatch(showLoading());
+    API.request('/no_operation', data).then(success, failure);
+  },
+
   generateFooter: function () {
     var {factory, hr} = this.props;
     var {day, hr} = convertHrtoDays(hr);
     return (
       <div id="menu-footer">
       <Row>
-        <Column className="menu-detail" small={12}>Day {day}, hr {hr}</Column>
-        <Column className="menu-detail" small={5}>₹ {factory.money}</Column>
+        <Column className="menu-detail" id="skip-turn" onClick={this.handleClick} small={5}>Skip Turn</Column>
+        <Column className="menu-detail" small={7}>Day {day}, hr {hr}</Column>
         <Column className="menu-detail" small={7}>Score - {factory.user_score}</Column>
+        <Column className="menu-detail" small={5}>₹ {factory.money}</Column>
       </Row>
       </div>
     );
@@ -65,7 +97,9 @@ module.exports = connect(
       index: state.layoutDetails.CurrentIndex,
       space: state.layoutDetails.LayoutSpaces[state.layoutDetails.CurrentIndex],
       factory: state.factory,
-      hr: state.userDetails.hr
+      hr: state.userDetails.hr,
+      user_id: state.userDetails.user_id,
+      auth_token: state.userDetails.auth_token
     };
   }
 )(MenuElement);
